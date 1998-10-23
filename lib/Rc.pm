c65082cdf0c226236f0f44fb77116392c8ec16e3
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
1998-10-23 21:01:56 +0000

    Import of JPRIT/Rc-0.01 from CPAN.
    gitpan-cpan-distribution: Rc
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JPRIT/Rc-0.01.tar.gz
    gitpan-cpan-author:       JPRIT
    gitpan-cpan-maturity:     released

---
+++ lib/Rc.pm
@@ -0,0 +1,106 @@
+use strict;
+package Rc;
+use IO::Handle;
+use base ('Exporter','DynaLoader');
+use vars qw($VERSION @EXPORT_OK $OutputFH);
+$VERSION = '0.01';
+@EXPORT_OK = qw(walk set_output
+	       $OutputFH);
+$OutputFH = \*STDOUT;
+
+__PACKAGE__->bootstrap($VERSION);
+
+sub walk {
+    local $SIG{__WARN__} = sub {
+	if ($_[0] =~ m/^Deep recursion/) {
+	    #ignore
+	} else {
+	    warn $_[0]
+	}
+    };
+    &_walk;
+}
+
+sub set_output {
+    $OutputFH = shift;
+}
+
+no strict 'refs';
+for(qw(Undef WordX Cmd RedirX Pipe)) {
+    my $c = 'Rc::'.$_.'::ISA';
+    @{"$c"} = 'Rc::Node';
+}
+
+for (qw(UnaryCmd BinCmd Forin)) {
+    my $c = 'Rc::'.$_.'::ISA';
+    @{"$c"} = 'Rc::Cmd';
+}
+
+# WordX
+@Rc::Word::ISA = 'Rc::WordX';
+@Rc::Qword::ISA = 'Rc::WordX';
+
+# Cmd <= UnaryCmd
+for (qw(Bang Nowait Count Flat Rmfn Subshell Var Case)) {
+    my $c = 'Rc::'.$_.'::ISA';
+    @{"$c"} = 'Rc::UnaryCmd';
+}
+
+# Cmd <= BinCmd
+for (qw(Andalso Assign Backq Body Brace Concat Else Epilog
+	If Newfn Cbody Orelse Pre Args Switch Match Varsub While Lappend)) {
+    my $c = 'Rc::'.$_.'::ISA';
+    @{"$c"} = 'Rc::BinCmd';
+}
+
+# Node <= RedirX
+for (qw(Dup Redir Nmpipe)) {
+    my $c = 'Rc::'.$_.'::ISA';
+    @{"$c"} = 'Rc::RedirX';
+}
+
+package Rc::Undef;
+sub type { 'undef' }
+
+package Rc::Nmpipe;
+
+*fd = \&Rc::Redir::fd;
+*to = \&Rc::Redir::to;
+
+1;
+__END__
+
+=head1 NAME
+
+Rc - parser and backends for 'rc' shell
+
+=head1 SYNOPSIS
+
+If you need a synposis, you should start with something easier.
+
+=head1 DESCRIPTION
+
+Isn't written yet.
+
+=head1 SUPPORT
+
+Send any questions or comments to envy@listbox.com!
+
+If you'd like to subscribe to this mailing list send email to
+majordomo@listbox.com.  Thanks!
+
+=head1 AUTHOR
+
+Copyright © 1998 Joshua Nathaniel Pritikin.  All rights reserved.
+
+This package is free software and is provided "as is" without express
+or implied warranty.  It may be used, redistributed and/or modified
+under the terms of the Perl Artistic License (see
+http://www.perl.com/perl/misc/Artistic.html)
+
+Portions of this sortware include source code from the 'rc' shell.
+These portions are Copyright © 1991 Byron Rakitzis.  'rc' is free,
+open-source package and is available at most ftp sites that distribute
+GNU software.
+
+=cut
